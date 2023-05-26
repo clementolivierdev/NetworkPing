7a9d72cff0e59b9f4500cbde0abe51329bdb587f
Clement Olivier <clement.olivier@eiffage.com>
2023-05-26 08:43:02 +0200

    feat[#3]: permet de scan un type d equipement en particulier

--- src/main/java/com/eiffage/pingnetwork/Lanceur.java
+++ src/main/java/com/eiffage/pingnetwork/Lanceur.java
@@ -1,5 +1,6 @@
 package com.eiffage.pingnetwork;
 
+import com.eiffage.pingnetwork.entities.Camera;
 import com.eiffage.pingnetwork.services.CommandLineInterfaceService;
 import com.eiffage.pingnetwork.services.ConvertisseurIPService;
 import com.eiffage.pingnetwork.vue.IHM;
@@ -15,8 +16,9 @@ public class Lanceur {
     public static void main(String[] args) {
         CommandLineInterfaceService commandLineInterfaceService = new CommandLineInterfaceService();
         ConvertisseurIPService convertisseurIPService = new ConvertisseurIPService();
+        Camera camera = new Camera();
         IHM ihm = new IHM();
-        ihm.setClasses(commandLineInterfaceService, convertisseurIPService);
+        ihm.setClasses(commandLineInterfaceService, convertisseurIPService, camera);
         ihm.setVisible(true);
     }
 
