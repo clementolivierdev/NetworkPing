4a4f3706af553f68adc7d2e68918e4b2476f02d7
Clement Olivier <clement.olivier@eiffage.com>
2023-05-30 09:46:57 +0200

    fix[#16]: regexIP améliorée

--- src/main/java/com/eiffage/pingnetwork/services/RegexService.java
+++ src/main/java/com/eiffage/pingnetwork/services/RegexService.java
@@ -11,17 +11,25 @@ import java.util.regex.Pattern;
 public class RegexService {
 
     /**
-     * Permet de vérifier si le String utilisé contient uniquement des Chiffres et des points
+     * Permet de vérifier si le String utilisé contient uniquement des Chiffres
+     * et des points
+     *
      * @param ip Adresse IP
      * @return Boolean vérification
      */
     public boolean verifierIP(String ip) {
         String[] splitIP = ip.split("\\.");
-        return Pattern.matches("[0-9.]+", ip) && splitIP.length==4;
+        for (String split : splitIP) {
+            if (split.length() > 3 || Integer.parseInt(split) <= 0 || Integer.parseInt(split) >= 255) {
+                return false;
+            }
+        }
+        return Pattern.matches("[0-9.]+", ip) && splitIP.length == 4;
     }
 
     /**
      * Permet de vérifier si le String utilisé contient uniquement des Chiffres
+     *
      * @param nombre Chiffre correspondant à une carte réseau
      * @return Boolean vérification
      */
